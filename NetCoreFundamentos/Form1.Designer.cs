e3401b78fc5e70f46aaf9d7c1d8038f73bdb5483
adrinetor86 <adrinetor81@gmail.com>
2025-12-12 10:21:52 +0100

    viernes

--- NetCoreFundamentos/Form1.Designer.cs
+++ NetCoreFundamentos/Form1.Designer.cs
@@ -28,18 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            label1 = new Label();
+            txtNombre = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(61, 124);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 35);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(61, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Introduzca su nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(40, 68);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(189, 23);
+            txtNombre.TabIndex = 2;
+            txtNombre.TextChanged += textBox1_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(811, 461);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox txtNombre;
     }
 }
--- NetCoreFundamentos/Form1.cs
+++ NetCoreFundamentos/Form1.cs
@@ -6,5 +6,22 @@ namespace NetCoreFundamentos
         {
             InitializeComponent();
         }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SOLO ESCRIBIMOS AQUI
+            this.txtNombre.Location = new Point(10,30);
+            this.txtNombre.Text = "Soy un texto nuevo!!!";
+            this.txtNombre.Width = 220;
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
+            this.txtNombre.BackColor = Color.Fuchsia;
+        }
     }
 }
